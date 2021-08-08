d436f02a6e556fab77ef387793caebd73dd9ab17
dqnguyen1506 <44854519+dqnguyen1506@users.noreply.github.com>
2021-08-08 15:37:05 -0700

    Restructure

---
+++ src/components/whiteboard/styles.js
@@ -0,0 +1,21 @@
+const styles = theme => ({
+    main: {
+        backgroundColor: "#353b48",
+        color: "white",
+        textAlign: "center",
+        padding: 25,
+        margin: "auto"
+    },
+
+    board: {
+        margin: 20,
+        width: "20%",
+    },
+
+    button: {
+        margin: "auto",
+        width: "10%",
+    }
+})
+
+export default styles;
