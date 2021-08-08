981f812253b54ad41053935103b976f57e791329
dqnguyen1506 <44854519+dqnguyen1506@users.noreply.github.com>
2021-08-08 15:42:21 -0700

    updated

---
+++ server.js
@@ -0,0 +1,104 @@
+const config = require('./config');
+const express = require('express');
+const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
+const { chatToken, videoToken, voiceToken } = require('./tokens');
+require('dotenv').config();
+const Pusher = require('pusher');
+
+const app = express();
+const port =  8080;
+const whiteboardPort = process.env.PORT || 4000;
+
+// whiteboard feature using pusher
+const pusher = new Pusher({
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.PUSHER_KEY,
+    secret: process.env.PUSHER_SECRET,
+    cluster: 'us3',
+  });
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  });
+  
+  app.post('/paint', (req, res) => {
+      console.log(req.body);
+      pusher.trigger('painting', 'draw', req.body);
+      res.json(req.body);
+    });
+  
+  app.listen(whiteboardPort, () => {
+    console.log(`Whiteboard Server started on port ${whiteboardPort}`);
+  });
+  // end of whiteboard feature
+
+  //video server
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(pino);
+
+const sendTokenResponse = (token, res) => {
+  res.set('Content-Type', 'application/json');
+  res.send(
+    JSON.stringify({
+      token: token.toJwt()
+    })
+  );
+};
+
+app.get('/api/greeting', (req, res) => {
+  const name = req.query.name || 'World';
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+});
+
+app.get('/chat/token', (req, res) => {
+  const identity = req.query.identity;
+  const token = chatToken(identity, config);
+  sendTokenResponse(token, res);
+});
+
+app.post('/chat/token', (req, res) => {
+  const identity = req.body.identity;
+  const token = chatToken(identity, config);
+  sendTokenResponse(token, res);
+});
+
+app.get('/video/token', (req, res) => {
+  const identity = req.query.identity;
+  const room = req.query.room;
+  const token = videoToken(identity, room, config);
+  sendTokenResponse(token, res);
+});
+
+app.post('/video/token', (req, res) => {
+  const identity = req.body.identity;
+  const room = req.body.room;
+  const token = videoToken(identity, room, config);
+  sendTokenResponse(token, res);
+});
+
+app.get('/voice/token', (req, res) => {
+  const identity = req.body.identity;
+  const token = voiceToken(identity, config);
+  sendTokenResponse(token, res);
+});
+
+app.post('/voice/token', (req, res) => {
+  const identity = req.body.identity;
+  const token = voiceToken(identity, config);
+  sendTokenResponse(token, res);
+});
+
+app.listen(3001, () =>
+  console.log('Express server is running on localhost:3001')
+);
+
