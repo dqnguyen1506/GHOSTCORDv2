4237730a0728420dcc3bf75d733b3717eab4167c
dqnguyen1506 <44854519+dqnguyen1506@users.noreply.github.com>
2021-08-09 09:46:49 -0700

    Update FriendsList.js

--- src/components/friends/FriendsList.js
+++ src/components/friends/FriendsList.js
@@ -19,30 +19,91 @@ import {
 import DeleteIcon from "@material-ui/icons/Delete";
 import ChatIcon from '@material-ui/icons/Chat';
 import {GoToChatComponent} from "../groupchat/goToChat";
-import { AnimatedList } from 'react-animated-list';
-import AlertComponent from '../groupchat/Alert';
+
+/* custom Badge (w/o animation) for user OFFLINE status */
+const StyledBadge = withStyles((theme) => ({
+    badge: {
+        backgroundColor: "gray",
+        color: "gray",
+        boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+        "&::after": {
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            borderRadius: "50%",
+            border: "1px solid currentColor",
+            content: '""',
+        },
+    },
+    "@keyframes ripple": {
+        "0%": {
+            transform: "scale(.8)",
+            opacity: 1,
+        },
+        "100%": {
+            transform: "scale(2.4)",
+            opacity: 0,
+        },
+    },
+}))(Badge);
+
+/* custom Badge (with animation) for user ONLINE status */
+const StyledBadge2 = withStyles((theme) => (
+    {
+    badge: {
+      backgroundColor: '#44b700',
+      color: '#44b700',
+      boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+      '&::after': {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        borderRadius: '50%',
+        animation: '$ripple 1.2s infinite ease-in-out',
+        border: '1px solid currentColor',
+        content: '""',
+      },
+    },
+    '@keyframes ripple': {
+      '0%': {
+        transform: 'scale(.8)',
+        opacity: 1,
+      },
+      '100%': {
+        transform: 'scale(2.4)',
+        opacity: 0,
+      },
+    },
+  }))(Badge);
 
 const FriendsListComponent = ({selectFunction, width}) => {
-    /* create delay to ensure dialog sliding animation */
+    /* a delay variable */
     const delay = ms => new Promise(res => setTimeout(res, ms));
+    // global context
     const { state, dispatch } = useContext(GlobalContext);
+    // list of user's friends objects
     const [friends, setFriends] = useState([]);
-    const [email, setEmail] = useState("");
-    const [name, setName] = useState("");
+    // variable to open goToChat component
     const [open, setOpen] = useState(false);
-    
+    // handler for closing goToChat component
     const handleClose = async () => {
         setOpen(false);
         await delay(500);
     };
-
+    //upon deleting a friend from user's friend list...
     const deleteFriend = async(email, name) => {
+        //if an alert is opened already, close it
         if(state.display.showAlert === true){
             await dispatch({
                 type: "SET_SHOW_ALERT",
                 payload: false,
             });
         };
+        // update alert information to be displayed
         if(state.alert.type !== 'deleted'){
             await dispatch({
                 type: "SET_ALERT_TYPE",
@@ -55,6 +116,7 @@ const FriendsListComponent = ({selectFunction, width}) => {
                 payload: 'User removed from Friends!',
             });
         };
+        // update user's friends list (remove selected user from list)
         await firestore
             .collection("users")
             .doc(state.user.email)
@@ -64,7 +126,10 @@ const FriendsListComponent = ({selectFunction, width}) => {
                     name: name,
                 }),
             })
-            .then(() => {});
+            .catch(error =>{
+                console.log(`Error during removal of selected user from friends list on Firebase: `, error)
+            })
+        // update the selected user's friend list (remove the current user from list)
         await firestore
             .collection("users")
             .doc(email)
@@ -74,34 +139,16 @@ const FriendsListComponent = ({selectFunction, width}) => {
                     name: auth.currentUser.displayName,
                 }),
             })
-            .then(() => {});
+            .catch(error =>{
+                console.log(`Error during your removal from selected user' friends list on Firebase: `, error)
+            })
         await dispatch({
             type: "SET_SHOW_ALERT",
             payload: true,
         });
-
-        // firestore
-        //     .collection("users")
-        //     .where("email", "==", state.user.email)
-        //     .get()
-        //     .then((res) => {
-        //         setFriends(res.docs[0].data().friends);
-        //         var friends = res.docs[0].data().friends;
-        //             var loopData = ''
-        //             for(var x in friends){
-        //                 const email = friends[x].email;
-        //                 loopData += `${email} `
-        //             }
-        //             dispatch({ 
-        //                 type: "SET_FRIENDS", 
-        //                 payload: loopData.split(" ")
-        //             });
-        //     });
-            
     };
     /* store new chat infomation in database */
     const createChat = async (name, email) => {
-        setName(email);
         await firestore
             .collection("chats")
             .add({
@@ -134,7 +181,7 @@ const FriendsListComponent = ({selectFunction, width}) => {
 
     /* handle chat with selected friend */
     const goToChat = async (name, email) => {
-        /* check if the 1-on-1 chat existed */
+        /* check if the 1-on-1/direct messaging chat existed */
         let directChats = state.home.chats.filter((chat) => chat.type ==='1on1');
         let res = state.home.chats.filter((chat) => 
             chat.users.includes(email) && 
@@ -155,7 +202,7 @@ const FriendsListComponent = ({selectFunction, width}) => {
                 });
             }
         }
-        /* if it doesn't exist, create one and prompt dialog to go to chat*/
+        /* if it doesn't exist, create one and prompt dialog to open goToChat component*/
         else{
             await createChat(name, email);
             dispatch({ 
@@ -173,18 +220,19 @@ const FriendsListComponent = ({selectFunction, width}) => {
         }
         
     };
+    // update local friends list upon relogging
     useEffect(() => {
         firestore
             .collection("users")
             .where("email", "==", state.user.email)
             .get()
             .then((res) => {
-                setFriends(res.docs[0].data().friends);   
-                // setFriends([...friends, res.docs[0].data().friends]);      
+                setFriends(res.docs[0].data().friends);         
             });
        
     }, [state.user.email, state.user.friends]);
 
+    //open goToChat component upon new direct messages chat
     useEffect(() => {
         if(state.display.showGoToChat === true){
             setOpen(true);
@@ -209,67 +257,6 @@ const FriendsListComponent = ({selectFunction, width}) => {
         }
     }, [state.display.showAlert]);
     
-    /* Badge (w/o animation) for user OFFLINE status */
-    const StyledBadge = withStyles((theme) => ({
-        badge: {
-            backgroundColor: "gray",
-            color: "gray",
-            boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-            "&::after": {
-                position: "absolute",
-                top: 0,
-                left: 0,
-                width: "100%",
-                height: "100%",
-                borderRadius: "50%",
-                border: "1px solid currentColor",
-                content: '""',
-            },
-        },
-        "@keyframes ripple": {
-            "0%": {
-                transform: "scale(.8)",
-                opacity: 1,
-            },
-            "100%": {
-                transform: "scale(2.4)",
-                opacity: 0,
-            },
-        },
-    }))(Badge);
-
-    /* Badge (with animation) for user ONLINE status */
-    const StyledBadge2 = withStyles((theme) => (
-        {
-        badge: {
-          backgroundColor: '#44b700',
-          color: '#44b700',
-          boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-          '&::after': {
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-            borderRadius: '50%',
-            animation: '$ripple 1.2s infinite ease-in-out',
-            border: '1px solid currentColor',
-            content: '""',
-          },
-        },
-        '@keyframes ripple': {
-          '0%': {
-            transform: 'scale(.8)',
-            opacity: 1,
-          },
-          '100%': {
-            transform: 'scale(2.4)',
-            opacity: 0,
-          },
-        },
-      }))(Badge);
-
-
     return (
         <div>
             <Grid 
@@ -278,28 +265,16 @@ const FriendsListComponent = ({selectFunction, width}) => {
                 justify='center'
                 alignItems='center'
             >
-                {/* <AlertComponent
-                type='success' 
-                message='User removed from Friends!'   
-            /> */}
-                {/* <AnimatedList animation={"grow"}> */}
+                {/* friends list */}
                 <List style = {{width: `${width} `}}>
                     {friends !== undefined ? (
                         friends.map((users, index) => {
                             return (
                                 <>
-                                    {/* <List style = {{width: `${width} `}}> */}
                                         <ListItem 
                                             key={index}
                                             style={{height:75}}>
-                                            {/* <GoToChatComponent
-                                                selectFunction={selectFunction}
-                                                name = {name}
-                                                email = {email}
-                                            >
-                            
-                                            </GoToChatComponent> */}
-                                                {/* if friend is online, display online badge */}
+                                                {/* if friend is ONLINE, display online badge w/ avatar */}
                                                 {state.home.statuses[users.email] === "online" ?
                                                     <React.Fragment>
                                                         <ListItemAvatar>
@@ -324,9 +299,8 @@ const FriendsListComponent = ({selectFunction, width}) => {
                                                         <ListItemText primary={users.name} secondary = {"online"} />
                                                     </React.Fragment>   
                                                 : 
-                                                
                                                     <React.Fragment>
-                                                        {/* if friend is offline, display offline badge */} 
+                                                        {/* if friend is offline, display offline badge w/ avatar */} 
                                                         <ListItemAvatar>
                                                             <StyledBadge
                                                                 style={{ }}
@@ -347,17 +321,12 @@ const FriendsListComponent = ({selectFunction, width}) => {
                                                             </StyledBadge>
                                                     
                                                         </ListItemAvatar>
+                                                        {/* friend username + online status */}
                                                         <ListItemText primary={users.name} secondary = {"offline"} />
                                                     </React.Fragment>   
                                                 }
                                                 <ListItemSecondaryAction>
-                                                    {/* <IconButton
-                                                edge="end"
-                                                aria-label="delete"
-                                                onClick={() => sendRequest(users.email)}
-                                            >
-                                                <AddIcon />
-                                            </IconButton> */}
+                                                    {/* for friends list displayed in Friends tab, there is a delete button */}
                                                     {width === "50vw" ? (
                                                         <IconButton
                                                             edge="end"
@@ -373,6 +342,7 @@ const FriendsListComponent = ({selectFunction, width}) => {
                                                         </IconButton>
                                                     ) : 
                                                         <React.Fragment>
+                                                            {/* for friends list displayed in main chat page, there is a chat button */}
                                                             <IconButton
                                                                 edge="end"
                                                                 aria-label="goToChat"
@@ -383,6 +353,7 @@ const FriendsListComponent = ({selectFunction, width}) => {
                                                             >
                                                                 <ChatIcon />
                                                             </IconButton> 
+                                                            {/* display goToChat component upon selecting chat button */}
                                                             {state.display.showGoToChat ? (
                                                                 <GoToChatComponent
                                                                     selectFunction={selectFunction}
@@ -418,7 +389,6 @@ const FriendsListComponent = ({selectFunction, width}) => {
                             <em>Add some friends!</em>
                         </div>
                     )}
-                {/* </AnimatedList> */}
                 </List>
             </Grid>
         </div>
